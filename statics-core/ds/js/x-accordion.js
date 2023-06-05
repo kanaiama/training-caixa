74f9567c50fd535f8ab478f316a07d451dc78f4e
kanaiama <wilson.kanaiamajr@sptech.school>
2023-06-05 14:23:21 -0300

    Removendo static-core

--- statics-core/ds/js/x-accordion.js
+++
@@ -1,8 +0,0 @@
-var xAccordion = {
-  toggle: function (button) {
-    var accordion = button.parentElement;
-    accordion.hasAttribute("active")
-      ? accordion.removeAttribute("active")
-      : accordion.toggleAttribute("active");
-  },
-};
