95819c5cbe8231b6a55ea7c6976efd87995d4b40
kanaiama <wilson.kanaiamajr@sptech.school>
2023-06-06 13:44:33 -0300

    Término da página de termo de contrato

--- mobile/tela-convênio/js/buttonChk.js
+++ mobile/tela-convênio/js/buttonChk.js
@@ -4,7 +4,9 @@ const button = document.getElementById('btnNext');
 checkbox.addEventListener('change', function() {
   if (checkbox.checked) {
     button.removeAttribute('disabled');
+    button.classList.add('enabled');
   } else {
     button.setAttribute('disabled', true);
+    button.classList.remove('enabled');
   }
 });
