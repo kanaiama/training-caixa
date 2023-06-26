70405c941388706c0c4882bd041c127428604204
kanaiama <wilson.kanaiamajr@sptech.school>
2023-06-26 17:54:30 -0300

    atualizando inputs para nao receber letras e chars especiais

--- mobile/progress-bar/js/prazoCarencia.js
+++ mobile/progress-bar/js/prazoCarencia.js
@@ -2,7 +2,7 @@ function editarPrazo(tipo) {
     var img_campo = document.getElementById("img_campo_" + tipo);
     var campoMes = document.getElementById("ipt_mes_" + tipo);
 
-    if(img_campo.src.match("edit.png")) {
+    if (img_campo.src.match("edit.png")) {
         img_campo.src = "img/ok.png";
         campoMes.disabled = false;
         campoMes.style.border = "1px solid green";
@@ -10,5 +10,42 @@ function editarPrazo(tipo) {
         img_campo.src = "img/edit.png"
         campoMes.disabled = true;
         campoMes.style.border = "none";
+    }
+}
+
+
+const mesMin = document.querySelector("#ipt_mes_min");
+const mesMax = document.querySelector("#ipt_mes_max");
+
+
+mesMin.addEventListener("keypress", function (e) {
+    // const keyCode = (e.keyCode ? e.keyCode : e.wich);
+
+    if(!checkChar(e)) {
+        e.preventDefault()
+    }
+    // if (keyCode > 64 && keyCode < 91 || keyCode == 48) {
+    //     e.preventDefault();
+    // }
+});
+
+mesMax.addEventListener("keypress", function (e) {
+    // const keyCode = (e.keyCode ? e.keyCode : e.wich);
+
+    if(!checkChar(e)) {
+        e.preventDefault()
+    }
+    // if (keyCode > 64 && keyCode < 91 || keyCode == 48) {
+    //     e.preventDefault();
+    // }
+});
+
+
+function checkChar(e) {
+    const char = String.fromCharCode(e.keyCode);
+    const pattern = '[Z1-9]'
+
+    if(char.match(pattern)) {
+        return true;
     }
 }
