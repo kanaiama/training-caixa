9e000f1aa77e3673bbccaef91a96b994675eb797
kanaiama <wilson.kanaiamajr@sptech.school>
2023-06-30 15:09:33 -0300

    Finalizando barra de progressão

--- mobile/progress-bar/js/prazoCarencia.js
+++ mobile/progress-bar/js/prazoCarencia.js
@@ -1,20 +1,27 @@
-function editarPrazo(tipo) {
-    let img_campo = document.getElementById("img_campo_" + tipo);
-    let campoMes = document.getElementById("ipt_mes_" + tipo);
+function editarPrazo() {
+    let img_campo = document.querySelector("#img_campo_max");
+    let campoMesMin = document.getElementById("ipt_mes_min");
+    let campoMesMax = document.getElementById("ipt_mes_max");
     let progresso = document.getElementById("slider-range-min");
 
-    // Atualiza a imagem, o campo e a barra de progresso
+    // Atualiza a imagem, o campo e a barra de progMresso
     if (img_campo.src.match("ok.png")) {
         img_campo.src = "img/edit.png";
-        campoMes.disabled = true;
-        campoMes.style.border = "none";
+        campoMesMin.disabled = true;
+        campoMesMin.style.border = "none";
+
+        campoMesMax.disabled = true;
+        campoMesMax.style.border = "none";
 
         progresso.style.pointerEvents = "auto";
         progresso.style.opacity = 1;
     } else {
         img_campo.src = "img/ok.png";
-        campoMes.disabled = false;
-        campoMes.style.border = "1px solid green";
+        campoMesMin.disabled = false;
+        campoMesMin.style.border = "1px solid green";
+
+        campoMesMax.disabled = false;
+        campoMesMax.style.border = "1px solid green";
 
         progresso.style.pointerEvents = "none";
         progresso.style.opacity = 0.5;
@@ -31,24 +38,40 @@ function editarPrazo(tipo) {
     }
 
     // Validação para garantir que o campo mínimo ou máximo não esteja vazio
-    if (tipo === 'min' && document.getElementById('ipt_mes_min').value === '') {
+    if (isNaN(min) || min === null || min === '') {
         alert('O campo mínimo não pode ficar vazio.');
-        min = 1; // Define um valor mínimo padrão
-        campoMes.value = 1; // Atualiza o valor exibido no campo
+        img_campo.src = "img/ok.png";
+        campoMesMin.disabled = false;
+        campoMesMin.style.border = "1px solid green";
+
+        campoMesMax.disabled = false;
+        campoMesMax.style.border = "1px solid green";
+
+        progresso.style.pointerEvents = "none";
+        progresso.style.opacity = 0.5;
     }
 
-    if (tipo === 'max' && document.getElementById('ipt_mes_max').value === '') {
+    if (isNaN(max) || max === null || max === '') {
+        
         alert('O campo máximo não pode ficar vazio.');
-        max = min + 1; // Define um valor máximo padrão
-        document.getElementById("ipt_mes_max").value = max;
+        img_campo.src = "img/ok.png";
+        campoMesMin.disabled = false;
+        campoMesMin.style.border = "1px solid green";
+
+        campoMesMax.disabled = false;
+        campoMesMax.style.border = "1px solid green";
+
+        progresso.style.pointerEvents = "none";
+        progresso.style.opacity = 0.5;
     }
+
 }
 
 
 
 let form = document.querySelector("#min-max")
 let mesMin = document.querySelector("#ipt_mes_min");
-let mesMax = document.querySelector("#ipt_mes_max");    
+let mesMax = document.querySelector("#ipt_mes_max");
 let warning = document.querySelector('#text-alert');
 
 
